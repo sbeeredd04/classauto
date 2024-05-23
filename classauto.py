69c91c2d6ad81dfbac5c697db60f066dd26d1780
Sri Ujjwal Reddy Beereddy <113922053+sbeeredd04@users.noreply.github.com>
2024-05-23 17:58:01 +0000

    selenium setup

--- classauto.py
+++ classauto.py
@@ -19,48 +19,16 @@ service = Service('C:\\Users\\sriuj\\classauto\\chromedriver-win64\\chromedriver
 driver = webdriver.Chrome(service=service)
 driver.get('https://webapp4.asu.edu/myasu/')
 
-# Login with username and password
-username = driver.find_element(By.XPATH, '//*[@id="username"]')
-username.send_keys(os.getenv('COLLEGE_USERNAME'))
-password = driver.find_element(By.XPATH, '//*[@id="password"]')
-password.send_keys(os.getenv('COLLEGE_PASSWORD'))
-password.send_keys(Keys.RETURN)
+#credentials
+name = 'REDACTED_USERNAME'
+password = 'Sriujjwal@0410'
 
-# Wait for the Duo authentication page to load
-WebDriverWait(driver, 2).until(
-    EC.presence_of_element_located((By.XPATH, '//*[@id="duo_iframe"]'))
-)
+# Login
+username = driver.find_element_by_id('username')
+username.send_keys(name)
+password = driver.find_element_by_id('password')
+password.send_keys(password)
 
-# Switch to the Duo iframe
-duo_iframe = driver.find_element(By.XPATH, '//*[@id="duo_iframe"]')
-driver.switch_to.frame(duo_iframe)
 
-# Click on "Enter a Passcode" tab
-passcode_tab = WebDriverWait(driver, 1).until(
-    EC.element_to_be_clickable((By.XPATH, '//*[@id="passcode"]'))
-)
-passcode_tab.click()
 
-# Generate the OTP code
-totp = pyotp.TOTP(os.getenv('TOTP_SECRET'))
-otp_code = totp.now()
 
-# Enter the OTP code
-otp_input = driver.find_element(By.XPATH, '//*[@id="auth_methods"]/fieldset/div[3]/div/input')
-otp_input.send_keys('514621')
-
-#clicking on the remember me button
-remember_me = driver.find_element(By.XPATH, '//*[@id="login-form"]/div[2]/div/label/input')
-remember_me.click()
-
-otp_input.send_keys(Keys.RETURN)
-
-# Save cookies to a file
-with open('cookies.pkl', 'wb') as file:
-    pickle.dump(driver.get_cookies(), file)
-
-# Wait for Duo authentication to complete
-time.sleep(5)  
-
-# Close WebDriver
-driver.quit()
